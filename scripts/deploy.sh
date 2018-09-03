f482c6b5cce24189e277ec8385a0502c6ef4dd72
Gilles Gouaillardet <gilles@rist.or.jp>
2018-09-03 13:57:32 +0900

    directly generate the doc into the gh-pages branch, and add comments
    Signed-off-by: Gilles Gouaillardet <gilles@rist.or.jp>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 #
-# Copyright (c) 2018 Cisco Systems, Inc.  All rights reserved.
+# Copyright (c) 2018      Research Organization for Information Science
+#                         and Technology (RIST). All rights reserved.
+# Copyright (c) 2018      Cisco Systems, Inc.  All rights reserved.
 #
 # $COPYRIGHT$
 #
 # Additional copyrights may follow
 #
 
-git clone --depth=1 -b gh-pages https://github.com/open-mpi/mtt.git gh-pages
-tar -C docs -cf - . | tar -C gh-pages -xf -
-
 cd gh-pages
+
+# latex doc contains some timestamps so it cannot be used to check if the doc has changed,
+# so only check the html file.
+# add the html directory to the index in order to include the newly created files/directories
 
 git add html
 
@@ -18,6 +21,7 @@ if git diff --cached --exit-code html > /dev/null 2>&1; then
     echo NOTHING TO PUSH
 else
     echo PUSHING CHANGES
+    # some changes were detected, push everything including the html directory
     git add .
     git commit -m "Deploy updated open-mpi/mtt to gh-pages"
     git push https://$GH_TOKEN@github.com/open-mpi/mtt.git gh-pages
